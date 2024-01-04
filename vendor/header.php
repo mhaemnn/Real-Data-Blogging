b34595ce616bbf4a64ee5bc6e0df8642b639a003
mhaemnn <muhaemin1998@gmail.com>
2024-01-04 22:57:32 +0700

    create header for navbar

--- vendor/header.php
+++ vendor/header.php
@@ -1,1 +1,29 @@
-<header></header>
+<header>
+  <a href="" class="logo-wrapper td-none">
+    <div><span>Real</span>Data</div>
+  </a>
+  <nav>
+    <ul class="navmenu">
+      <li class="navitem"><a href="#">All Post</a></li>
+      <li class="navitem"><a href="#">Life Lession</a></li>
+      <li class="navitem"><a href="#">Jurnals</a></li>
+      <li class="navitem">
+        <a href="#">Best Articles</a>
+        <ul class="dropdown">
+          <li><a href="#">Best of 2021</a></li>
+          <li><a href="#">Best of 2022</a></li>
+          <li><a href="#">Best of 2023</a></li>
+        </ul>
+      </li>
+      <li class="navitem"><a href="#">Register</a></li>
+      <li class="navitem"><a href="#">Login</a></li>
+      <li class="navitem">
+        <a href="#">muhaemin</a>
+        <ul class="dropdwon">
+          <li><a href="#">Dashboard</a></li>
+          <li><a href="#">Logout</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+</header>
