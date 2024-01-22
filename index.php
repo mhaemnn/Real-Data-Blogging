8f90b12429a466f9e9d1030cefd1b7d8ccf6bd29
mhaemnn <muhaemin1998@gmail.com>
2024-01-22 23:09:30 +0700

    modifikasi

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="author" content="muhaemin">
+  <title>Ordering | Dashboard</title>
+  <!-- bootstrap online -->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <!-- bootstrap online -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <!-- Custom styles for this template -->
+  <link href="./assets/css/style.css" rel="stylesheet">
+  <!-- mystyle -->
+  <link rel="stylesheet" href="./assets/css/main.css">
+</head>
+
+<body>
+  <?php include "./vendor/header.php"; ?>
+  <!-- header -->
+  <div class="container-fluid">
+    <div class="row">
+      <!-- sidebar -->
+      <?php include "./vendor/sidebar.php"; ?>
+      <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+
+      </main>
+    </div>
+  </div>
+
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <script src="./assets/js/main.js"></script>
+</body>
+
+</html>
